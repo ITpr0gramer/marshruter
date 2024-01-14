35247550794c7fbebf5a96e241c637a24af3444f
Luka <ggflyte@mail.ru>
2024-01-14 12:31:56 +0300

    first

---
+++ src/views/ContactView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="Contact">
+    <h1>А здесь будут наши контакты</h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="Project">
+    <h1>Добро пожаловать на наш проект!</h1>
+  </div>
+</template>
